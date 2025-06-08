178a275c2751a2c4ead44569139c78723a57d64b
Long Nguyenle <107901701+lognguyenle@users.noreply.github.com>
2025-06-08 00:47:24 -0700

    deploy basepath

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
-const repoName = 'website'; // ⬅️ update this
+const repoName = 'website';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true,
   },
