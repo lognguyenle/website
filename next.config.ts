f453d4f588225cb31ef7589561b9ea8dc0905fe6
Long Nguyenle <107901701+lognguyenle@users.noreply.github.com>
2025-06-07 23:11:49 -0700

    image fix 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const repoName = 'website'; // ⬅️ update this
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
