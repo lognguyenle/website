d17c4ff760fe00c0a3b54fa28ecba2fd641a44a3
Long Nguyenle <107901701+lognguyenle@users.noreply.github.com>
2025-06-08 00:39:13 -0700

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavigationBar from "./navbar";
 import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "longlonglo.ng",
+  title: "Long Nguyenle",
   description: "A personal website, made for CSE 461",
 };
 
