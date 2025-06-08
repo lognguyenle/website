5d60be5f67c10438295f4af9440c477b2d762906
Long Nguyenle <107901701+lognguyenle@users.noreply.github.com>
2025-06-07 22:39:09 -0700

    basically done

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         </div>
         <div className="flex flex-col m-4 items-start">
           <div className="text-4xl font-serif underline">Hi! I'm Long Nguyenle</div>
-          <div className="w-100">I am currently a student studying Computer Science at the University of Washington. </div>
+          <div className="w-100">I am currently a student studying Computer Science at the University of Washington and an incoming SDE Intern at AWS. <br/> I am always trying to find ways to improve my life and believe that consistency is key. <br/> I love and persistantly chase the feeling of finally intuitively understanding the underworkings of complicated mechanisms. <br/> Though I say all of that, I wish I had more time to focus on my numerous hobbies, and goals. <br/><br/> <a href="https://open.spotify.com/playlist/6OhWCsGQF1saCP8dNDf7Wp?si=213c3486686a4096" className="text-blue-400 underline">If you enjoy JPop I think you will like my playlist here!</a></div>
         </div>
       </div>
     </div>
